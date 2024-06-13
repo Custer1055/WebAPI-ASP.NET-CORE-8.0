e75b7b92b94424bd04b85170f0c8635b9df208ac
Custer Murwamuila <custer.murwamuila@weconnect.tech>
2024-06-13 14:56:51 +0200

    initial commit

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Model;
+
+namespace WebAPI.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPI.Model.Product> Product { get; set; } = default!;
+        public DbSet<WebAPI.Model.Category> Category { get; set; } = default!;
+    }
+}
---
+++ Migrations/20240519133915_init2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "categoryid",
+                table: "Product");
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.AddColumn<int>(
+                name: "categoryid",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
